de745d42626739dcecdd67119d9d2d140f0e8b66
srikanthpragada <srikanthpragada@gmail.com>
2017-01-29 12:18:58 +0530

    29-JAN

---
+++ WebContent/services/MyServices.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module('MyServices', []);
+
+app.service('DateService',
+    // provide constructor function, which creates an object 
+    function () {
+        this.message = "Date Service!";
+        this.year = function () {
+            var today = new Date();
+            return today.getFullYear();
+        };
+    }
+);
+
+
+app.factory('TimeService',
+    // factory function that returns an instance of service 
+    function () {
+        return {
+            'hours': new Date().getHours(),
+            'minutes': new Date().getMinutes(),
+            'seconds': new Date().getSeconds(),
+            getTime : function() {
+                return new Date().toString();
+            }
+        }
+    }
+);
+
+
+
+
